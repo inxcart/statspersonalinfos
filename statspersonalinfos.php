9c6566cab71560545b7be3b33a7511e09f4ce848
Quetzacoalt91 <thomas.nabord@prestashop.com>
2014-03-27 17:20:38 +0100

    / MO statspersonalinfos : min ps_versions_compliancy modified (1.5.6.1 => 1.6)

--- statspersonalinfos.php
+++ statspersonalinfos.php
@@ -36,7 +36,7 @@ class StatsPersonalInfos extends ModuleGraph
 	{
 		$this->name = 'statspersonalinfos';
 		$this->tab = 'analytics_stats';
-		$this->version = 1.1;
+		$this->version = '1.2';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
@@ -44,7 +44,7 @@ class StatsPersonalInfos extends ModuleGraph
 
 		$this->displayName = $this->l('Registered customer information');
 		$this->description = $this->l('Adds information about your registered customers (such as gender and age) to the Stats dashboard.');
-		$this->ps_versions_compliancy = array('min' => '1.5.6.1', 'max' => _PS_VERSION_);
+		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
 	}
 
 	public function install()
