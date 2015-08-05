814006f39cd29550b8174ffa9535ad2be950c960
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-08-05 09:34:47 +0200

    // Version updated to v1.4.0

--- statspersonalinfos.php
+++ statspersonalinfos.php
@@ -36,7 +36,7 @@ class StatsPersonalInfos extends ModuleGraph
 	{
 		$this->name = 'statspersonalinfos';
 		$this->tab = 'analytics_stats';
-		$this->version = '1.3.1';
+		$this->version = '1.4.0';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
